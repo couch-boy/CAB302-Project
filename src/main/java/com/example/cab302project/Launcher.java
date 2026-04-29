af20749442ca900e9a24f6fae4262020833bbaf9
Maan <mistrymaan2710@gmail.com>
2026-04-29 22:37:54 +1000

    Added comments on the branches i worked and added a few comments where there missing (Maan's work)

--- src/main/java/com/example/cab302project/Launcher.java
+++ src/main/java/com/example/cab302project/Launcher.java
@@ -2,6 +2,7 @@ package com.example.cab302project;
 
 import javafx.application.Application;
 
+
 public class Launcher {
     public static void main(String[] args) {
         Application.launch(HelloApplication.class, args);
